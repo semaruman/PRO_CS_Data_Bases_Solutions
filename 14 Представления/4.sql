7784c651f69919ab8d4e69794b3ef66d4e934443
Semen <semen.ruman@gmail.com>
2026-03-06 06:59:31 +0300

    Решил 4 задачу в разделе "Представления"

--- 14 Представления/4.sql
+++ 14 Представления/4.sql
@@ -4,5 +4,6 @@ SELECT u.id, u.full_name, GROUP_CONCAT(c.title ORDER BY c.created_date DESC SEPA
 FROM users u
 JOIN certificates cer ON u.id = cer.user_id
 JOIN courses c ON cer.course_id = c.id
+WHERE u.is_active
 GROUP BY u.id, u.full_name
 ORDER BY full_name
